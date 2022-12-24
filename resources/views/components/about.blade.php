0af7cd5b9a529d44d7a8e8bec7eb45bcb6cf2cb2
Daltonic <darlingtongospel@gmail.com>
2022-12-24 12:14:21 +0100

    reworks the about component

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col justify-center items-center space-y-4">
+<div class="flex flex-col justify-center items-center space-y-4 w-4/5 mx-auto mt-16">
     <h1 class="text-white text-4xl font-black">Artwork Collection</h1>
     <p class="text-[#14ec5d] text-xs">
         Mint and collect the hottest NFTs around.
@@ -13,13 +13,13 @@
         class="flex flex-row justify-center space-x-2 items-center bg-[#000000ad] rounded-full my-4 pr-3 cursor-pointer">
         <img class="w-11 h-11 object-cover object-top rounded-full"
             src="https://avatars.githubusercontent.com/u/107491357?v=4" alt="" />
-            
+
         <div class="flex flex-col font-semibold">
             <span class="text-white text-sm">0xf55...146a</span>
             <span class="text-[#14ec5d] text-xs">Queen</span>
         </div>
     </a>
     <button class="bg-[#14ec5d] py-2 px-4 text-xs font-medium rounded-sm">
-        Mint
+        Mint NFT
     </button>
 </div>
